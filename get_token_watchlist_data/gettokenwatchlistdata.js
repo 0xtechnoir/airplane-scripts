fcc4cafb5850e9b3dddf699f710de53bffde4766
0xNaylor <0xnaylor@gmail.com>
2023-05-15 07:48:28 +0100

    Updated script to remove formatting before writing to db. Formatting will be done application-side

--- get_token_watchlist_data/gettokenwatchlistdata.js
+++ get_token_watchlist_data/gettokenwatchlistdata.js
@@ -7,15 +7,38 @@ const cryptoRankAPIKey = process.env.CRYPTORANK_API_KEY;
 const client = new MongoClient(URL);
 
 export default async function() {
-  
+
+/*
+Token IDs:
+
+MANA  121
+SAND  21407
+ILV   29958
+GALA  23865
+AXS   26691
+RNDR  7481
+ENJ   112
+WAXP  105
+AURY  170039
+MYTH  178125
+PRIME 181246
+APE   173608
+MAGIC 169601
+RONIN 172730
+UOS   7847
+DAR   170227
+SWEAT 176027
+RAINI 50207
+*/
+
   try {
     
     await client.connect()
     const db = client.db("historical_price_data")
-    const tokens = 'MANA,SAND,ILV,GALA,AXS,RNDR,ENJ,WAXP'
-    const response = await axios.get(`https://api.cryptorank.io/v1/currencies?api_key=${cryptoRankAPIKey}&symbols=${tokens}`)
-    
-    
+    coinIDs = '121,21407,29958,23865,26691,7481,112,105,170039,178125,181246,173608,169601,172730,7847,170227,176027,50207'
+    // const response = await axios.get(`https://api.cryptorank.io/v1/currencies?api_key=${cryptoRankAPIKey}&symbols=${tokens}`)
+    const response = await axios.get(`https://api.cryptorank.io/v1/currencies?api_key=${cryptoRankAPIKey}&ids=${coinIDs}`)
+  
     const data = response.data.data  
     for(let i=0 ; i< data.length; i++) {
       
@@ -26,29 +49,38 @@ export default async function() {
         maxSupply = data[i].maxSupply
       }
       
-      const percChange24h = data[i].values.USD.percentChange24h > 0 ? `${data[i].values.USD.percentChange24h.toFixed(2)}%` : `(${Math.abs(data[i].values.USD.percentChange24h.toFixed(2))}%)`
-      const percChange7d = data[i].values.USD.percentChange7d > 0 ? `${data[i].values.USD.percentChange7d.toFixed(2)}%` : `(${Math.abs(data[i].values.USD.percentChange7d.toFixed(2))}%)`
-      const percChange30d = data[i].values.USD.percentChange30d > 0 ? `${data[i].values.USD.percentChange30d.toFixed(2)}%` : `(${Math.abs(data[i].values.USD.percentChange30d.toFixed(2))}%)`
-      const percChange3m = data[i].values.USD.percentChange3m > 0 ? `${data[i].values.USD.percentChange3m.toFixed(2)}%` : `(${Math.abs(data[i].values.USD.percentChange3m.toFixed(2))}%)`
-      const percChange6m = data[i].values.USD.percentChange6m > 0 ? `${data[i].values.USD.percentChange6m.toFixed(2)}%` : `(${Math.abs(data[i].values.USD.percentChange6m.toFixed(2))}%)`
-      
+      const percChange24h = data[i].values.USD.percentChange24h >= 0 ? `${data[i].values.USD.percentChange24h.toFixed(2)}%` : `(${Math.abs(data[i].values.USD.percentChange24h.toFixed(2))}%)`
+      const percChange7d = data[i].values.USD.percentChange7d >= 0 ? `${data[i].values.USD.percentChange7d.toFixed(2)}%` : `(${Math.abs(data[i].values.USD.percentChange7d.toFixed(2))}%)`
+      const percChange30d = data[i].values.USD.percentChange30d >= 0 ? `${data[i].values.USD.percentChange30d.toFixed(2)}%` : `(${Math.abs(data[i].values.USD.percentChange30d.toFixed(2))}%)`
+      const percChange3m = data[i].values.USD.percentChange3m >= 0 ? `${data[i].values.USD.percentChange3m.toFixed(2)}%` : `(${Math.abs(data[i].values.USD.percentChange3m.toFixed(2))}%)`
+      const percChange6m = data[i].values.USD.percentChange6m >= 0 ? `${data[i].values.USD.percentChange6m.toFixed(2)}%` : `(${Math.abs(data[i].values.USD.percentChange6m.toFixed(2))}%)`
+      const price = data[i].values.USD.price
+      const vol24h = data[i].values.USD.volume24h
+      const mc = data[i].values.USD.marketCap
+      const circSupply = data[i].circulatingSupply
+      const totalSupply = data[i].totalSupply
+
+      console.log("Token Name: ", data[i].symbol.toUpperCase())
+      console.log("Circ Supply: ", data[i].circulatingSupply)
+
       const document = {
         "coin_id": data[i].id,
         "token": data[i].symbol.toUpperCase(),
         "rank": data[i].rank,
-        "price": Intl.NumberFormat().format(data[i].values.USD.price),
+        "price": price,
         "price_change_pc_24hr": percChange24h,
         "price_change_pc_7d": percChange7d,
         "price_change_pc_30d": percChange30d,
         "price_change_pc_3m": percChange3m,
         "price_change_pc_6m": percChange6m,
-        "volume_24h": abbreviate(data[i].values.USD.volume24h, 2, false, false),
-        "market_cap": abbreviate(data[i].values.USD.marketCap, 2, false, false),
-        "fdv": fdv ? abbreviate(fdv, 2, false, false) : '-',
-        "circulating_supply": abbreviate(data[i].circulatingSupply, 2, false, false),
-        "total_supply" : abbreviate(data[i].totalSupply, 2, false, false),
-        "max_supply": maxSupply ? abbreviate(parseInt(data[i].maxSupply), 2, false, false) : '-',
-        "pc_circulating": maxSupply ? ((data[i].circulatingSupply / data[i].maxSupply) * 100).toFixed(0) : '100'
+        "volume_24h": vol24h ? vol24h : null,
+        "market_cap": mc ? mc : null,
+        "fdv": fdv ? fdv : null,
+        // "circulating_supply": abbreviate(data[i].circulatingSupply, 2, false, false),
+        "circulating_supply": circSupply ? circSupply : null,
+        "total_supply" : totalSupply ? totalSupply : null,
+        "max_supply": maxSupply ? maxSupply : null,
+        "pc_circulating": (maxSupply && circSupply && maxSupply != 0) ? ((circSupply / maxSupply) * 100) : null
       }
 
       const query = { coin_id: data[i].id }
